fb2572719dc891125b79303dab151ecd3aa1bf89
Macbucheron1 <nathandeprat@hotmail.fr>
2025-05-31 13:24:22 +0200

    Feat: adding zsh

--- home/mac.nix
+++ home/mac.nix
@@ -2,15 +2,22 @@
   home.username = "mac";
   home.homeDirectory = "/home/mac";
 
+  home.packages = with pkgs; [ 
+    zsh 
+  ];
+
   programs.git = {
     enable = true;
     userName = "Macbucheron1";
     userEmail = "nathan@example.com";
   };
 
-  home.packages = with pkgs; [
-
-  ];
+  programs.zsh = {
+    enable = true;
+    enableCompletion = true;
+    syntaxHighlighting.enable = false;
+    autosuggestions.enable = false;
+  };
 
   home.stateVersion = "24.05";
 }
