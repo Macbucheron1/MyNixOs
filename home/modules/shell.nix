a168a763a9046dd0e3e832884f953acc334f1527
Macbucheron1 <nathandeprat@hotmail.fr>
2025-05-31 15:35:55 +0200

    Feat: first ricing on starship

--- home/modules/shell.nix
+++ home/modules/shell.nix
@@ -6,9 +6,10 @@
 
   programs.starship = {
     enable = true;
+	
     settings = {
       add_newline = false;
-      format = "$directory $character";
+      format = "$directory $git_branch $git_status $character";
     };
   };
 
