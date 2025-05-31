e1afa31d46cdb5e795821eaeb648b15aaa0be986
Macbucheron1 <nathandeprat@hotmail.fr>
2025-05-31 15:51:06 +0200

    Feat: adding nixupdate command

--- home/modules/shell.nix
+++ home/modules/shell.nix
@@ -2,11 +2,23 @@
   programs.zsh = {
     enable = true;
     enableCompletion = true;
+    initContent = ''
+      alias nixupdate='
+        if [[ -n $(git -C /etc/nixos status --porcelain) ]]; then
+          echo "❌ Git tree is dirty. Please commit your changes before rebuilding.";
+          echo ""
+          git -C /etc/nixos status
+	  return 1;
+        else
+          echo "✅ Git tree is clean. Proceeding with rebuild...";
+          sudo nixos-rebuild switch --flake /etc/nixos#mac-nixos;
+        fi
+      '
+    '';
   };
 
   programs.starship = {
     enable = true;
-	
     settings = {
       add_newline = false;
       format = "$directory $git_branch $git_status $character";
