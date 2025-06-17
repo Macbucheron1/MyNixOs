0bf6c20d4ce3ba095677719d592451658009b30f
Macbucheron1 <nathandeprat@hotmail.fr>
2025-06-17 19:21:13 +0200

    fix: fixing secrets

--- modules/secrets.nix
+++ modules/secrets.nix
@@ -19,10 +19,6 @@
       "/home/mac/.ssh/id_ed25519"
     ];
     
-    # Chemin vers le fichier qui définit quelles clés publiques peuvent déchiffrer quels secrets
-    # Ce fichier est utilisé par la commande 'agenix' lors du chiffrement
-    secretsFile = ../secrets/secrets.nix;
-    
     # La définition des secrets sera ajoutée ici au fur et à mesure
     # de vos besoins. Les secrets peuvent être référencés dans d'autres
     # modules via config.age.secrets.<nom>.path
